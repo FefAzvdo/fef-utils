7f08a7169f066a1d1dd9e7fc3aa8e21227feab66
FefAzvdo <wfernandoaugusto01@gmail.com>
2020-06-23 18:50:38 -0300

    Drawer closeing onMask click

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState, Fragment } from 'react'
-import { Row, Col, Button } from 'fef-utils'
 import {
   StyledDrawerShadowMask,
   StyledDrawerBody,
@@ -28,15 +27,33 @@ export const Drawer = ({
 
   useEffect(() => {
     if (!isOpen) {
-      return () => onOpen()
-    } else return () => onClose()
+      return () => {
+        onOpen()
+        setIsDrawerOpen(true)
+      }
+    } else
+      return () => {
+        onClose()
+        setIsDrawerOpen(false)
+      }
   }, [isOpen])
 
+  const handleOpenState = () => {
+    if (!isOpen) {
+      setIsDrawerOpen(true)
+    } else onClose()
+    setIsDrawerOpen(false)
+  }
+
   const [height, setHeight] = useState(0)
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
 
   return (
     <Fragment>
-      <StyledDrawerShadowMask isOpen={isOpen} />
+      <StyledDrawerShadowMask
+        isOpen={(isOpen = isDrawerOpen)}
+        onClick={handleOpenState}
+      />
       <StyledDrawerBody isOpen={isOpen} height={height}>
         <StyledDrawerHeader>{title}</StyledDrawerHeader>
         <div style={{ padding: '10px' }}>{children}</div>
